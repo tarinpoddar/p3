d9d09ddd927f9362e6b15cbf8569cd04e49126aa
Tarin Poddar <tarinpoddar27@gmail.com>
2014-07-15 16:37:46 -0400

    made minor styling changes

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,10 +5,20 @@
 
 <h1 class="headings"> Developer's Best Friend </h1>
 
-<div class="links">
-<a href="/lorem_ipsum_generator"> Lorem Ipsum Generator </a>  <br>
-<a href="/random_user_generator"> Random User Generator </a>
+<div class="section">
+	<p class="links"> <a class="links" href="/lorem_ipsum_generator"> Lorem Ipsum Generator </a>  </p> <br>
+		<p class="text">
+			It generates random text for you. <br>
+			You choose the number of paragraphs you want and it generates for you.
+ 		</p>
+</div>
 
+<div class="section">
+	<p class="links"> <a href="/random_user_generator"> Random User Generator </a> </p> <br>
+		<p class="text">
+			It creates random users for you. <br> You can choose how many users
+			you want and how much detail of the user do you require.
+		</p>
 </div>
 
 @stop
--- app/views/random_user_generator.blade.php
+++ app/views/random_user_generator.blade.php
@@ -8,24 +8,31 @@
 
 @section('content')
 
-<h1> Random User Generator </h1>
-
+@if (!isset($data))
+	<h1 class="headings"> Random User Generator </h1>
+	<p class="info"> To generate random users, please retype the link in your brower. <br>
+		 Add a slash in the current link, write the number of users you want and press enter </p>
+@endif
 
 @if (isset($data))
+	<h1 class="headings"> Here are your random users </h1>
 
 	@for ($i = 1; $i < ($data[0]+1); $i++)
-    
+    <p class="userinfo">
 		@for ($j = 0; $j < 3; $j++)
 
+		
 			{{ $data[$i][$j] }} <br>
-
+		
 		@endfor
-
+	</p>
+	
+	
 		{{ HTML::image($data[$i][3]) }} <br>
-
+	
 
 	@endfor
-
+	
 
 @endif
 
